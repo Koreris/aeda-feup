158730df71ad0ed1445efd14ee03aa8f4475b22c
Koreris <mariadsda@gmail.com>
2016-11-20 14:42:16 +0000

    more tweaks

--- Person.cpp
+++ Person.cpp
@@ -98,26 +98,23 @@ bool RegPerson::getHasVehicle() const
 
 
 
-vector<RegPerson*> RegPerson::getBuddies() const
+vector<RegPerson*>& RegPerson::getBuddies()
 {
 	return buddies;
 }
 
-vector<Vehicle *> RegPerson::getVehicles() const
+vector<Vehicle *>& RegPerson::getVehicles()
 {
 	return vehicles;
 }
 
 //crud
-bool RegPerson::areMutualBuddies(const RegPerson* other_person) const
+bool RegPerson::areMutualBuddies(RegPerson* other_person) const
 {
-
-	vector <RegPerson *> other_p_buddies=other_person->getBuddies();
-	return (find(other_p_buddies.begin(),other_p_buddies.end(), this) != other_p_buddies.end());
-
+	return (find(other_person->getBuddies().begin(),other_person->getBuddies().end(), this) != other_person->getBuddies().end());
 }
 
-vector<string> RegPerson::getNotifications() const
+vector<string>& RegPerson::getNotifications()
 {
 	return notifications;
 }
