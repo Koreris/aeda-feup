628b308b9659d8713a7ee4319725bd6f5f4c9e4b
Koreris <mariadsda@gmail.com>
2016-11-20 14:47:55 +0000

    altered Logic.cpp, added vectors, getters & setters
    IT ALL COMES TUMBLING DOWN TUMBLING DOWN

--- Logic.cpp
+++ Logic.cpp
@@ -11,6 +11,16 @@ using namespace std;
 Logic::Logic()
 {}
 
+/**
+ * Returns the current date, provided by the Date class
+ * @brief Returns the Library's current date and time
+ * @return Date with the current date and time
+ */
+Date Logic::get_curDate() const
+{
+	return Date::curDate();
+}
+
 bool Logic::userLogin(string usr, string passw)
 {
 	return (usr == "test" && passw == "1234");
@@ -23,15 +33,65 @@ vector<Trip *> Logic::findDest(string dest)
 	//returns true and
 }
 
+vector<Trip*>& Logic::getCurTrips ()
+	{
+		return cur_trips;
+	}
 
+void Logic::setCurTrips (vector<Trip*>& curTrips)
+	{
+		cur_trips = curTrips;
+	}
 
+vector<Place*>& Logic::getDelDestinations()
+{
+	return del_destinations;
+}
 
+void Logic::setDelDestinations (vector<Place*>& delDestinations)
+{
+	del_destinations = delDestinations;
+}
 
+vector<Person*>& Logic::getDelRegUsers ()
+{
+	return del_regUsers;
+}
 
+void Logic::setDelRegUsers (vector<Person*>& delRegUsers)
+{
+	del_regUsers = delRegUsers;
+}
 
+vector<Trip*>& Logic::getDelTrips ()
+{
+	return del_trips;
+}
 
+void Logic::setDelTrips (vector<Trip*>& delTrips)
+{
+	del_trips = delTrips;
+}
 
+vector<Place*>& Logic::getDestinations()
+{
+	return destinations;
+}
 
+void Logic::setDestinations (vector<Place*>& destinations)
+{
+	this->destinations = destinations;
+}
+
+vector<Person*>& Logic::getRegUsers ()
+{
+	return regUsers;
+}
+
+void Logic::setRegUsers (vector<Person*>& regUsers)
+{
+	this->regUsers = regUsers;
+}
 
 
 
--- Logic.h
+++ Logic.h
@@ -11,12 +11,42 @@
 
 using namespace std;
 
+
 class Logic
 {
+private:
+	vector<Person *> regUsers;
+	vector<Person *> del_regUsers;
+	vector<Place *> destinations;
+	vector<Place *> del_destinations;
+	vector<Trip *> cur_trips;
+	vector<Trip *> del_trips;
+
 public:
 	Logic();
 	bool userLogin(string usr, string passw);
 	vector<Trip *> findDest(string dest);
+	vector<Trip *> tripSortByDate();
+
+	Date get_curDate() const;
+
+	vector<Trip*>& getCurTrips ();
+	void setCurTrips (vector<Trip*>& curTrips);
+
+	vector<Place*>& getDelDestinations ();
+	void setDelDestinations (vector<Place*>& delDestinations);
+
+	vector<Person*>& getDelRegUsers ();
+	void setDelRegUsers (vector<Person*>& delRegUsers);
+
+	vector<Trip*>& getDelTrips ();
+	void setDelTrips (vector<Trip*>& delTrips);
+
+	vector<Place*>& getDestinations ();
+	void setDestinations (vector<Place*>& destinations);
+
+	vector<Person*>& getRegUsers ();
+	void setRegUsers (vector<Person*>& regUsers);
 };
 
 #endif //LOGIC_H_
